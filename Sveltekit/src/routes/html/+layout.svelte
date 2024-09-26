2932153609bfd455766f4144d5e26789f88a69ee
Vice06 <06jovi@skola.engelholm.se>
2024-09-26 13:37:08 +0200

    first commit

---
+++ Sveltekit/src/routes/html/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Header from "../../lib/components/Header.svelte";
+
+
+</script>
+<Header />
+<slot />
+
+<h1>LAYOUT</h1>
+<style>
+    h1{
+        color: blue;
+    }
+</style>
